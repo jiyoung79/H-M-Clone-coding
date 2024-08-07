42c3acd9f5692cc7bfef0128367018bdfcf6a4b2
jiyoung79 <kangjiyoung9879@gmail.com>
2024-08-07 16:41:26 +0900

    login_page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
          </div>
          <div className='nav_search'>
             <FontAwesomeIcon icon={faSearch} />
-            <input type='text' />
+            <input type='text' className='search_input'/>
          </div>
       </div>
    );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Login = () => {
-   const navigate = useNavigate();
-   
-   return <div>로그인 페이지</div>;
+   return (
+      <div className='login_form1'>
+         <div className='login_form2'>
+            <h2>로그인 LOGIN</h2>
+            <form>
+               <div className='id'>
+                  <label>아이디</label>
+                  <input type='text' name='name' placeholder='아이디를 입력해주세요.'></input>
+               </div>
+               <div className='password'>
+                  <label>비밀번호</label>
+                  <input type='password' name='password' placeholder='비밀번호를 입력해주세요.'></input>
+               </div>
+               <div className='find_idpassword'>
+                  <Link to='/' className='find_id'>
+                     아이디 찾기
+                  </Link>
+                  <label>|</label>
+                  <Link to='/' className='find_password'>
+                     비밀번호 찾기
+                  </Link>
+               </div>
+               <div className='login_btn'>
+                  <Button variant='secondary' size='lg' className='btn'>
+                     로그인
+                  </Button>
+               </div>
+            </form>
+         </div>
+      </div>
+   );
 };
 
 export default Login;
