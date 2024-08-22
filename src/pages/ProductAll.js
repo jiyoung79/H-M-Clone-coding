3e7f98b9dac2e1ee328bc242ebd51cd6e86546d8
jiyoung79 <kangjiyoung9879@gmail.com>
2024-08-22 23:16:14 +0900

    complete3

--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -9,7 +9,7 @@ const ProductAll = () => {
    const getProducts = async () => {
       let searchQuery = query.get('q') || '';
       console.log('쿼리값은? : ', searchQuery);
-      let url = `http://localhost:4000/products?q=${searchQuery}`;
+      let url = `https://my-json-server.typicode.com/jiyoung79/H-M-Clone-coding/products?q=${searchQuery}`;
       let response = await fetch(url);
       let data = await response.json();
       console.log(data);
